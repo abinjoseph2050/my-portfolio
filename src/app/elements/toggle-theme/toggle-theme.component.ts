60b8b545918e9f2708956b07d7fa6dae46ec1c7b
abinjoseph2050 <abinjoseph2050@gmail.com>
2024-02-14 18:39:27 +0530

    ui updates

---
+++ src/app/elements/toggle-theme/toggle-theme.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle-theme',
+  standalone: true,
+  imports: [],
+  templateUrl: './toggle-theme.component.html',
+  styleUrl: './toggle-theme.component.scss'
+})
+export class ToggleThemeComponent {
+
+}
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { ToggleThemeComponent } from '../elements/toggle-theme/toggle-theme.component';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [ToggleThemeComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
